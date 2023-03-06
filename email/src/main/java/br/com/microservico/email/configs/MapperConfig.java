4e437eaeb07cfb5b8bcafabfc3c3f3a9740c8cb2
Jonathan Zapotosczny <JonathanGM.HD@gmail.com>
2023-03-06 12:37:50 -0300

    Adicionando ModelMapper...

---
+++ email/src/main/java/br/com/microservico/email/configs/MapperConfig.java
@@ -0,0 +1,14 @@
+package br.com.microservico.email.configs;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+    @Bean
+    public ModelMapper modelMapperConfig() {
+        return new ModelMapper();
+    }
+}
